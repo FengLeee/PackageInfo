908b64b45aa4a88e4f354902db33036c2f77724c
鲨鱼辣椒\Administrator <1711602280@qq.com>
2016-10-28 01:43:52 +0800

    明天看看listview更新的问题

--- src/com/yiya/info/MainActivity.java
+++ src/com/yiya/info/MainActivity.java
@@ -13,8 +13,11 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.text.format.Formatter;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -22,34 +25,65 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	TextView admin;
+	
+	private static final String TAG = "MainActivity";
 	TextView user;
 	ListView list;
 	List<List> info;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		user = (TextView) findViewById(R.id.user);
-		admin = (TextView) findViewById(R.id.admin);
+		
+		user.setVisibility(View.INVISIBLE);
+		
 		list = (ListView) findViewById(R.id.list);
 
 		// get package info
-		
-		info = getPackageInfo();
 
-		MyAdapter adapter = new MyAdapter();
-		list.setAdapter(adapter);
+		info = getPackageInfo();
+		
+		new Thread() {
+			public void run() {
+				MyAdapter adapter = new MyAdapter();
+				list.setAdapter(adapter);
+				setOnScrollListener();
+			};
+		}.start();
 
 	}
 
+	protected void setOnScrollListener() {
+		list.setOnScrollListener(new OnScrollListener() {
+			
+			@Override
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
+				
+			}
+			
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem,
+					int visibleItemCount, int totalItemCount) {	
+				if(  firstVisibleItem > 0 && firstVisibleItem< info.get(0).size()) {
+					user.setText("用户应用");
+					user.setVisibility(View.VISIBLE);
+				}else if(firstVisibleItem > info.get(0).size() + 1) {
+					user.setText("系统应用");
+					user.setVisibility(View.VISIBLE);
+				}
+			}
+		});
+		
+	}
+
 	private List<List> getPackageInfo() {
-		// ActivityManager am = (ActivityManager)
-		// getSystemService(ACTIVITY_SERVICE); 获得正在运行的
+		
 		PackageManager pm = getPackageManager();
 		List<PackageInfo> info = pm.getInstalledPackages(0);
-		// Toast.makeText(this, ""+info.size(), 0).show();
+		
 		List<List> list = new ArrayList<List>();
 		List<PackageInfoo> user = new ArrayList<PackageInfoo>();
 		List<PackageInfoo> admin = new ArrayList<PackageInfoo>();
@@ -59,7 +93,7 @@ public class MainActivity extends Activity {
 			Drawable icon = i.applicationInfo.loadIcon(pm);
 			String name = (String) i.applicationInfo.loadLabel(pm);
 
-			String file = i.applicationInfo.dataDir;
+			String file = i.applicationInfo.sourceDir;
 			File f = new File(file);
 			long l = f.length();
 			String size = Formatter.formatFileSize(this, l);
@@ -83,63 +117,72 @@ public class MainActivity extends Activity {
 		return list;
 
 	}
-	
+
 	static class ViewHolder {
-		  ImageView icon;
-		  ImageView loc;
-		  TextView name;
-		  TextView size;
+		 ImageView icon;
+		 ImageView loc;
+		 TextView name;
+		 TextView size;
 	}
-	
 
 	class MyAdapter extends BaseAdapter {
-
 		@Override
 		public int getCount() {
-			return info.get(0).size()+info.get(1).size()+2;
+			return info.get(0).size() + info.get(1).size() + 2;
 		}
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder holder = null;
 			View v = null;
-			PackageInfoo p;
-			if(position == 0) {
+			PackageInfoo p = null;
+			if (position == 0) {
 				TextView tv = new TextView(getApplicationContext());
 				tv.setText("用户应用");
 				return tv;
-			} if(position > 0 && position < info.get(0).size() + 1 ) {
-				//用户应用
+			}
+			if (position > 0 && position < info.get(0).size() + 1 ) {
+				// 用户应用
 				
-				//获得info
-				p = (PackageInfoo) info.get(0).get(position -1);
-				
-			} if (position == info.get(0).size() + 1) {
+				// 获得info
+				p = (PackageInfoo) info.get(0).get(position - 1);
+
+			}
+			if (position == info.get(0).size() + 1) {
 				TextView tv = new TextView(getApplicationContext());
 				tv.setText("系统应用");
 				return tv;
-			} if (position > info.get(0).size() + 1) {
-				//系统应用
-				p = (PackageInfoo) info.get(1).get(position -2-info.get(0).size());
 			}
-			
-			if(convertView != null && convertView instanceof RelativeLayout) {
-				v =  convertView;
-			}else {
+			if (position > info.get(0).size() + 1) {
+				// 系统应用
+				p = (PackageInfoo) info.get(1).get(
+						position - 2 - info.get(0).size());
+			}
+
+			if (convertView != null && convertView instanceof RelativeLayout) {
+
+				holder = (ViewHolder) convertView.getTag();
+
+				v = convertView;
+
+			} else {
 				v = View.inflate(getApplicationContext(), R.layout.item, null);
+
+				holder = new ViewHolder();
+
+				holder.icon = (ImageView)v.findViewById(R.id.ivl);
+				holder.loc = (ImageView)v.findViewById(R.id.ivr);
+				holder.name = (TextView)v.findViewById(R.id.name);
+				holder.size = (TextView)v.findViewById(R.id.size);
 				
-				ViewHolder holder = new ViewHolder();
-				holder.icon = (ImageView) findViewById(R.id.ivl);
-				holder.loc = (ImageView) findViewById(R.id.ivr);
-				holder.name	= (TextView) findViewById(R.id.name);
-				holder.size	= (TextView) findViewById(R.id.size);
 				v.setTag(holder);
-				
 			}
 			
-			
-			
-			
-			
+			holder.icon.setImageDrawable(p.getIcon());
+			holder.loc.setImageResource(p.getLoc());
+			holder.name.setText(p.getName());
+			holder.size.setText(p.getSize());
+
 			return v;
 		}
 
